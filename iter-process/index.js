b6185781eb51b00da06dff41c9da782b2c323632
Alexander Averin <averin.alexander@outlook.com>
2020-06-30 17:20:13 +0300

    fix resolve smallestDivisor

--- iter-process/index.js
+++ iter-process/index.js
@@ -1,15 +1,17 @@
 const smallestDivisor = (num) => {
-
+  
   const iter = (counter, acc) => {
-    if (acc % counter === 0 ) {
+    if (num === 1) {
+      return 1;
+    }
+    if (acc % counter === 0) {
       return counter;
     }
-    
-    return iter(counter + 1, acc);  
+    return iter(counter + 1, acc);
   };
 
   return iter(2, num);
 
 };
 
-console.log(smallestDivisor(17));
+smallestDivisor(15);
