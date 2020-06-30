486404c55e6a28a9b142b002191e8fb697795122
Alexander Averin <averin.alexander@outlook.com>
2020-06-30 14:23:43 +0300

    init

---
+++ iter-process/index.js
@@ -0,0 +1,10 @@
+const smallestDivisor = (num) => {
+    const iter = (counter, acc) => {
+      if (acc % counter >= 1) {
+        return acc;
+      }
+      return acc / iter(counter + 1, acc / counter);
+    };
+    return iter(2, num);
+  };
+  smallestDivisor(9);
