482abab7b49d5122d2720f63dbc7e1a21baac0a1
Alexander Averin <averin.alexander@outlook.com>
2020-07-17 16:45:30 +0300

    addDigits v1

--- addDigits/index.js
+++ addDigits/index.js
@@ -1,3 +1,17 @@
 const addDigits = (num) => {
+    let n = String(num);
+    let i = n.length;
+    let result = null;
+    let acc = null;
+    while (i > 0) {
+        i -= 1;
+        acc = n[i];
+        result += Number(acc);
+    }
+    const counter = (result) => {
+        while (i > 0) {
+            
+        }
+    }
 };
 console.log(addDigits(38));
