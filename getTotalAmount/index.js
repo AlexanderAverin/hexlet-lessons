cd3f8adda2b98817e05594f8ff7cb051c3bbd198
Alexander Averin <averin.alexander@outlook.com>
2020-08-12 11:06:04 +0300

    getTotalAmount final working version

--- getTotalAmount/index.js
+++ getTotalAmount/index.js
@@ -1,25 +1,12 @@
 const getTotalAmount = (arr, name) => {
     let result = 0;
     for (const item of arr) {
-        if (item === undefined || item === "") {
-            break;
-        } else if (name === "usd" && item.slice(0, 3) === "usd") {
-            if (item.slice(0, 3) !== "usd") {
-                continue;
-            }
-            result += Number(item.slice(4, item.length));
-        } else if (name === "eur" && item.slice(0, 3) === "eur") {
-            if (item.slice(0, 3) !== "eur") {
-                continue;
-            }
-            result += Number(item.slice(4, item.length));
-        } else if (name === "rub" && item.slice(0, 3) === "rub") {
-            if (item.slice(0, 3) !== "rub") {
-                continue;
-            }
-            result += Number(item.slice(4, item.length));
-        }
+        if (item !== undefined && item.slice(0, 3) === name) {
+            result += Number(item.slice(3));
+    } else if (item !== undefined && item.slice(0, 3) !== name) {
+        continue;
     }
+  }
     return result;
 };
-console.log(getTotalAmount(['eur 10', 'usd 1',, 'usd 5'], "usd"));
+console.log(getTotalAmount(['eur 10', 'usd 1',, 'usd 5'], "usd"))
