21724b4ee3fcae06bc83f970903553b97c226f59
Alexander Averin <averin.alexander@outlook.com>
2020-08-26 16:02:44 +0300

    countUniqChars working version

---
+++ countUniqChars/index.js
@@ -0,0 +1,11 @@
+const countUniqChars = (str) => {
+    const arr = str.split("");
+    const check = [];
+    for (const item of arr) {
+        if (arr.includes(item) === true && check.includes(item) === false) {
+            check.push(item);
+        }
+    }
+    return check.length;      
+};
+console.log(countUniqChars('You know nothing Jon Snow'));
