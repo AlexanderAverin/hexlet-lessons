4aede3a936f5d26a7e5ca70c37792b67376a8f74
Alexander Averin <averin.alexander@outlook.com>
2021-01-10 16:50:24 +0300

    getIntersectionOfSortedArrays/э

--- getIntersectionOfSortedArrays/index.js
+++ getIntersectionOfSortedArrays/index.js
@@ -3,26 +3,32 @@ const getIntersectionOfSortedArrays = (arr, arr2) => {
   let i = 0;
   let i2 = 0;
   if (arr.length > arr2.length) {
-    while (i2 <= arr2.length) {
+    while (i2 !==  arr2.length) {
       if (arr[i] === arr2[i2]) {
         result.push(arr[i]);
       } else if (i === arr.length) {
         i = 0;
         i2++;
-    }
-    i++;
-  }
-  } else if (arr2.length > arr.length) {
-      while (i <= arr.length) {
         if (arr2[i2] === arr[i]) {
           result.push(arr2[i2]);
+      }
+    }
+      i++;
+    }
+  } else if (arr2.length > arr.length) {
+    while (i !== arr.length) {
+      if (arr2[i2] === arr[i]) {
+        result.push(arr2[i2]);
       } else if (i2 === arr2.length) {
         i2 = 0;
         i++;
+        if (arr2[i2] === arr[i]) {
+          result.push(arr2[i2]);
       }
+    }
       i2++;
     }
   }
   return result;
 };
-console.log(getIntersectionOfSortedArrays([1, 2, 3], [2, 3, 4, 6]));
+console.log(getIntersectionOfSortedArrays([10, 12, 19, 21], [3, 5, 10]));
