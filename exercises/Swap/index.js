d4fc797e359819b6b4fa45881cec385587afea8f
Alexander Averin <averin.alexander@outlook.com>
2021-06-30 21:08:48 +0300

    Test

--- exercises/Swap/index.js
+++ exercises/Swap/index.js
@@ -1,12 +1,11 @@
 const swap = (array) => {
     if (array.length < 2) {
         return array;
-    } else {
+    }
         const first = array[0];
         const second = array[array.length - 1];
         array[0] = second;
         array[array.length - 1] = first;
-        return array;
-    }
+    return array;
 };
 console.log(swap([1, 2, 3, 4]));
