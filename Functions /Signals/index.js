a636310f664763642aa096e96e521f04d2144054
Alexander Averin <averin.alexander@outlook.com>
2021-07-06 11:55:44 +0300

    Many changes

---
+++ Functions /Signals/index.js
@@ -0,0 +1,28 @@
+var _ = require('lodash');
+
+const giveDomain = (emeil) => {
+
+};
+
+const getFreeDomainsCount = (emeils) => {
+
+};
+
+const freeEmailDomains = [
+  'gmail.com',
+  'yandex.ru',
+  'hotmail.com',
+];
+
+const emails = [
+  'info@gmail.com',
+  'info@yandex.ru',
+  'info@hotmail.com',
+  'mk@host.com',
+  'support@hexlet.io',
+  'key@yandex.ru',
+  'sergey@gmail.com',
+  'vovan@gmail.com',
+  'vovan@hotmail.com',
+];
+
