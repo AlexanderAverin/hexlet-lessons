1a16d476105cac378adf58960b8a9d81c5ef21c5
Alexander Averin <averin.alexander@outlook.com>
2021-07-06 11:57:43 +0300

    Another Changes

--- exercises/while/index.js
+++ exercises/while/index.js
@@ -1,13 +1,13 @@
 const smallestDivisor = (num) => {
     if (num === 1) {
-        return 1;
+    return 1;
     }
     if (num < 1) {
-      return NaN;
+    return NaN;
     }
     let counter = 2;
-    while (num % counter >= 1 ) {
-        counter = counter + 1;
+    while (num % counter >= 1) {
+        counter += 1;
     }
     return counter;
 };
