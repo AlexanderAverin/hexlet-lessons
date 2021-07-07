dc7df8e3398896ed7b4a1dc9c5c1e7e9c569d6e5
Alexander Averin <averin.alexander@outlook.com>
2021-07-07 13:05:40 +0300

    Add some exercies

--- Functions /Signals/index.js
+++ Functions /Signals/index.js
@@ -1,18 +1,33 @@
 var _ = require('lodash');
 
-const giveDomain = (emeil) => {
-
-};
-
-const getFreeDomainsCount = (emeils) => {
-
-};
-
 const freeEmailDomains = [
   'gmail.com',
   'yandex.ru',
   'hotmail.com',
 ];
+// First version
+const giveDomain = (email) => email.slice(email.indexOf('@') + 1, email.length);
+
+const getFreeDomainsCount = (emails) => emails
+  .filter((email) => freeEmailDomains.indexOf(giveDomain(email)) !== -1)
+  .reduce((acc, item) => {
+    const host = giveDomain(item);
+    const group = acc[host] ?? [];
+    return { ...acc, [host]: Number(group + 1) };
+  }, {});
+
+// Second version
+
+const getFreeDomainsCount = (emails) => emails
+  .filter((email) => {
+    const [name, domain] = email.split('@');
+    return domain;
+  }
+  .reduce((acc, item) => {
+    const host = giveDomain(item);
+    const group = acc[host] ?? [];
+    return { ...acc, [host]: Number(group + 1) };
+  }, {});
 
 const emails = [
   'info@gmail.com',
@@ -24,5 +39,8 @@ const emails = [
   'sergey@gmail.com',
   'vovan@gmail.com',
   'vovan@hotmail.com',
+  'Sasha@gmail.com',
+  'vova@icloud',
 ];
 
+console.log(getFreeDomainsCount(emails));
